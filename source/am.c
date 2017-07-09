dd498d4240d5b59fe0063b5d5df42dc4e2e5c455
JoycieC <tajoyciec@googlemail.com>
2017-07-09 03:25:48 +0100

    Update am.c

--- source/am.c
+++ source/am.c
@@ -37,7 +37,10 @@ static const u64 pokemonTitleIDs[] = {
 	0x0004000000171C00, ///< Pokémon Red	(EUR) (IT)
 	0x0004000000171D00, ///< Pokémon Blue	(EUR) (IT)
 	0x0004000000171E00, ///< Pokémon Yellow	(EUR) (IT)
-	0x00040000008D9200, ///< Pokémon Crystal(FREE) (IT)
+	0x0004000000000000, ///< Pokémon Gold (FREE) (EN)
+	0x0004000000000000, ///< Pokémon Silver (FREE) (EN)
+	0x0004000000000000, ///< Pokémon Crystal (FREE) (EN)
+
 	
 	0x0000000000000000,	///< Not Pokémon
 };
